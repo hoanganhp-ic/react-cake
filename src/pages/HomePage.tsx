32a4110c670d8e69f73abf260b0471b1d16059f6
PhamHoangAnh <hoanganh.pham@infcurion.com>
2024-07-14 22:48:26 +0900

    handeRemove

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,34 +4,47 @@ import { useState } from 'react';
 
 const HomePage = () => {
 
-const [cakes, setCakes] = useState<Cake[]>([
-    {
-        id: 1,
-        title: 'Bánh chưng',
-        description: 'Miền trung'
-    },
-    {
-        id: 2,
-        title: 'Bánh cá',
-        description: 'Cá hồi'
-    },
-    {
-        id: 3,
-        title: 'Bánh kem',
-        description: 'Sinh nhật'
-    },
-    {
-        id: 4,
-        title: 'Bánh mì',
-        description: 'Pate'
-    }
-]);
+    const [cakes, setCakes] = useState<Cake[]>([
+        {
+            id: 1,
+            title: 'Bánh chưng',
+            description: 'Miền trung'
+        },
+        {
+            id: 2,
+            title: 'Bánh cá',
+            description: 'Cá hồi'
+        },
+        {
+            id: 3,
+            title: 'Bánh kem',
+            description: 'Sinh nhật'
+        },
+        {
+            id: 4,
+            title: 'Bánh mì',
+            description: 'Pate'
+        }
+    ]);
+
+    const [person, setPerson] = useState<Cake>({
+        title: 'CPTho',
+        description: 'Hello'
+    });
+
+    const handleRemovePerson = (id: number | undefined) => {
+        const indexCake = cakes.findIndex(cake => cake.id === id);
+        let newCakes = [...cakes];
+        newCakes.splice(indexCake, 1);
+        setCakes(newCakes);
+    };
+
 
   return (
     <div style={{ height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflowY: 'auto'}}>
         <div className="wrapper-card-items">
             {
-                cakes.map(cake => <CardCake id={cake.id} title={cake.title} description={cake.description}/>)
+                cakes.map(cake => <CardCake key={cake.id} id={cake.id} title={cake.title} description={cake.description} handleRemoveItem={handleRemovePerson}/>)
             }
         </div>
     </div>
--- src/sections/CardCake.tsx
+++ src/sections/CardCake.tsx
@@ -1,21 +1,23 @@
 import LogoCake from '../assets/banh-chung-0218134.webp';
 import { Cake } from '../models/cake.model';
 
-type Props = Cake;
+type Props = Cake & {
+    handleRemoveItem: (id: number) => void;
+};
 
-const CardCake = ({title, description}: Props) => {
+const CardCake = ({id, title, description, handleRemoveItem}: Props) => {
     return (
-        <div className="card-items">
-        <img src={LogoCake} alt="logo cake" className="card-image-cake" />
-        <div style={{ height: '50%', width: '100%'}}></div>
-        <div className='card-content'>
-            <div>
+        <div className="card-items" onClick={() => handleRemoveItem(id!)}>
+            <img src={LogoCake} alt="logo cake" className="card-image-cake" />
+            <div style={{ height: '50%', width: '100%'}}></div>
+            <div className='card-content'>
                 <div>
-                    <div style={{fontSize: '20px', fontWeight: '600', color: '#fff'}}>{title}</div>
-                    <div style={{fontSize: '20px', fontWeight: '600', color: '#fff', marginTop: '0.5rem'}}>{description}</div>
+                    <div>
+                        <div style={{fontSize: '20px', fontWeight: '600', color: '#fff'}}>{title}</div>
+                        <div style={{fontSize: '20px', fontWeight: '600', color: '#fff', marginTop: '0.5rem'}}>{description}</div>
+                    </div>
                 </div>
             </div>
-        </div>
     </div>
     );
 }
