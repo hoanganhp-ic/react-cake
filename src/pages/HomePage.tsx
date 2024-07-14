82f303b04a79d2e69888d506dafbb6266634a967
PhamHoangAnh <hoanganh.pham@infcurion.com>
2024-07-15 02:34:31 +0900

    handle Search

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import SpinnerLoad from '../components/SpinnerLoad';
 import TextField from '../components/TextField';
 import { Cake } from '../models/cake.model';
 import CardCake from '../sections/CardCake';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 const HomePage = () => {
 
@@ -25,38 +25,70 @@ const HomePage = () => {
     const [page, setPage] = useState(1);
     const [cakes, setCakes] = useState<Cake[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [searchText, setSearchText] = useState<string>('');
+    const searchRef = useRef<any>(null);
 
     const handleShowMore = () => {
         setIsLoading(true);
         setPage(page + 1);
     }
+    
+    const handleSearch = (value: string) => {
+        clearTimeout(searchRef.current!);
+        searchRef.current = setTimeout(() => {
+            setSearchText(value);
+        }, 1500);
+    };
 
     useEffect(() => {
         setIsLoading(true);
-        const data = dataCakes.slice((page - 1) * 3, page * 3);
-        setTimeout(() => {
-            setCakes(prev => [...cakes, ...data]);
-            setIsLoading(false);
-        }, 1000);
-    }, [page]); 
+        if (searchText) {
+            if (page === 1) {
+                const data = searchCakes.slice(0, 3);
+                setTimeout(() => {
+                    setCakes(data);
+                    setIsLoading(false);
+                }, 1000);
+            } else {
+                const data = searchCakes.slice((page - 1) * 3, page * 3);
+                setTimeout(() => {
+                    setCakes(prev => [...cakes, ...data]);
+                    setIsLoading(false);
+                }, 1000);
+            
+            }
+        } else {
+            const data = dataCakes.slice((page - 1) * 3, page * 3);
+            setTimeout(() => {
+                setCakes(prev => [...cakes, ...data]);
+                setIsLoading(false);
+            }, 1000);
+        }
+    }, [page, searchText]);
+
+    const searchCakes = useMemo(() => {
+        setPage(1);
+        setCakes([]);
+        return dataCakes.filter(cake => cake.name?.toLowerCase().includes(searchText.toLowerCase()));
+    }, [searchText]);
 
 
   return (
     <div style={{ height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflowY: 'auto'}}>
         {
-            !cakes.length && (
-                <div style={{ display:'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
-                    <SpinnerLoad />
-                </div>
-            )
+            // (!cakes.length && !searchText) && (
+            //     <div style={{ display:'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+            //         <SpinnerLoad />
+            //     </div>
+            // )
         }
         {
-            cakes.length > 0 && (
+            (
                 <div>
-                    <TextField placeholder="Search cake" width='250px'/>
+                    <TextField placeholder="Search cake" width='250px' onChange={handleSearch}/>
                     <div className="wrapper-card-items">
                         {
-                            cakes.map(cake => <CardCake key={cake.id} id={cake.id} name={cake.name} description={cake.description} />)
+                            (cakes || []).map(cake => <CardCake key={cake.id} id={cake.id} name={cake.name} description={cake.description} />)
                         }
                         <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
                         <ButtonField loading={isLoading} onClick={handleShowMore}> Show more </ButtonField>
