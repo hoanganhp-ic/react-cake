868a2a5fc08080bb4344b876677e43ed13650f88
PhamHoangAnh <hoanganh.pham@infcurion.com>
2024-07-15 01:31:33 +0900

    handle button show more

--- src/components/ButtonField.tsx
+++ src/components/ButtonField.tsx
@@ -3,11 +3,12 @@ import { IcSpinner } from "../icons/IcSpinner";
 type Props = {
     loading?: boolean;
     children?: React.ReactNode;
+    onClick?: () => void;
 }
 
-const ButtonField = ({ children, loading }: Props) => {
+const ButtonField = ({ children, loading, onClick }: Props) => {
     return (
-        <div className="btn-main">
+        <div className="btn-main" onClick={onClick && onClick}>
             {!loading 
                 ? children 
                 : <div style={{ display: 'flex', alignItems: 'center', columnGap: '0.5rem', color: '#fff' }}>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import ButtonField from '../components/ButtonField';
 import TextField from '../components/TextField';
 import { Cake } from '../models/cake.model';
 import CardCake from '../sections/CardCake';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 const HomePage = () => {
 
@@ -10,14 +10,29 @@ const HomePage = () => {
         { id: 1, name: 'Bánh chưng', description: 'Miền trung' },
         { id: 2, name: 'Bánh cá', description: 'Cá hồi' },
         { id: 3, name: 'Bánh kem', description: 'Sinh nhật' },
-        { id: 4, name: 'Bánh mì', description: 'Pate' }
+        { id: 4, name: 'Bánh mì', description: 'Pate' },
+        { id: 5, name: 'Bánh gạo', description: 'Làm từ gạo lứt' },
+        { id: 6, name: 'Bánh cáy', description: 'Đặc sản Nghệ An' },
+        { id: 7, name: 'Bánh nem', description: 'Đến từ thanh hoá' },
+        { id: 8, name: 'Bánh cuốn', description: 'Bánh mướt nhân thịt' },
+        { id: 9, name: 'Bánh trứng', description: 'Trứng khủng long' },
+        { id: 10, name: 'Bánh bao', description: 'Bánh bao 1 trứng' },
+        { id: 11, name: 'Bánh gà', description: 'Bánh gà công nghiệp' },
+        { id: 12, name: 'Bánh nướng', description: 'Có chese' },
     ]
 
+    const [page, setPage] = useState(1);
     const [cakes, setCakes] = useState<Cake[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        setCakes(dataCakes);
-    }, []);
+        setIsLoading(true);
+        const data = dataCakes.slice((page - 1) * 3, page * 3);
+        setTimeout(() => {
+            setCakes(prev => [...cakes, ...data]);
+            setIsLoading(false);
+        }, 1000);
+    }, [page]); 
 
 
   return (
@@ -28,7 +43,7 @@ const HomePage = () => {
                 cakes.map(cake => <CardCake key={cake.id} id={cake.id} name={cake.name} description={cake.description} />)
             }
             <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
-            <ButtonField> Show more </ButtonField>
+            <ButtonField loading={isLoading} onClick={() => setPage(page + 1)}> Show more </ButtonField>
             </div>
         </div>
     </div>
