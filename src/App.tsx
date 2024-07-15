248e96c36140321c1d422342318c2d911da6a1c9
PhamHoangAnh <hoanganh.pham@infcurion.com>
2024-07-15 23:04:10 +0900

    implement router

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,37 @@ import Header from './layouts/Header';
 import HomePage from './pages/HomePage';
 import Footer from './layouts/Footer';
 import './App.css';
+import { createBrowserRouter, Outlet, RouteObject, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+
+const appRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'create'
+      }
+    ]
+  }
+];
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <Outlet />
+    ),
+    children: appRoutes
+  }
+])
 
 function App() {
 
-
-  return (
-    <div className="App">
-      <Header />
-      <HomePage />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
